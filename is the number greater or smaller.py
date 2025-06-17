80f1c4abd471263b863cdeea94a988ce66fb84a8
lunar_eclipse <sleepysmorz@gmail.com>
2025-06-17 19:06:53 +1200

    which number is bigger

---
+++ is the number greater or smaller.py
@@ -0,0 +1,8 @@
+i =int(input("enter a number:"))
+if (i < 15):
+    print("1 is smaller the 157468")
+    print("i'm in the if block")
+else:
+    print("1 is greater than 15")
+    print("i'm in the else block")
+print("i'm not in the if block or the else block yippeeeee i escaped the block prisons")
